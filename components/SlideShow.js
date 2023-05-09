366d484d36ba2c6aafb17ef7ff6edc075f88195f
Streamline6 <webstreamline13@gmail.com>
2023-05-09 21:43:45 +0530

    small changes

--- components/SlideShow.js
+++ components/SlideShow.js
@@ -16,17 +16,17 @@ const Slideshow = () => {
 
 	//These are custom properties for zoom effect while slide-show
 	const zoomInProperties = {
-		scale: 1.2,
+		scale: 1,
 		duration: 5000,
-		transitionDuration: 500,
+		transitionDuration: 300,
 		infinite: true,
 		prevArrow: (
-			<div className="ml-10">
+			<div className="ml-10 top-40 md:top-72">
 				<ArrowLeftIcon className="h-8 w-8 text-white cursor-pointer" />
 			</div>
 		),
 		nextArrow: (
-				<div className="mr-10">
+				<div className="mr-10 top-40 md:top-72">
 				   <ArrowRightIcon className="h-8 w-8 text-white cursor-pointer" />
 			    </div>
 		),
@@ -35,13 +35,13 @@ const Slideshow = () => {
 		<div className="w-full h-screen">
 			<Zoom {...zoomInProperties}>
 				{images.map((each, index) => (
-					<div key={index} className="flex justify-center items-center w-screen h-screen relative">
+					<div key={index} className="flex justify-center md:items-center items-start w-screen h-screen relative">
 						<img
 							className="w-screen"
 							src={each}
 						/>
-                        <h1 className="absolute top-60 inset-x-1/4 text-center z-10 text-6xl bold text-white">Hello, Nik</h1>
-                        <p className="absolute top-80 inset-x-1/4 text-center z-10 text-2xl bold text-white">"Everything you can imagine is real."</p>
+                        <h1 className="absolute md:top-60 top-24 inset-x-1/4 text-center z-10 md:text-6xl text-4xl bold text-white">Hello, Nik</h1>
+                        <p className="absolute md:top-80 top-40 inset-x-1/4 text-center z-10 md:text-2xl text-xl bold text-white">"Everything you can imagine is real."</p>
 					</div>
 				))}
 			</Zoom>
